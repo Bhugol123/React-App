12396c5ed21d64a8fb71aebd16385eccb173fcd0
Bhugol123 <bhugolkc07@gmail.com>
2024-07-04 20:05:18 -0700

    new branch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from "react";
-import Header from "./components/Header";
-import Displaysec from "./pages/Home/Displaysec";
-import Texh from "./pages/Home/Tech";
-import Footer from "./components/Footer";
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Displaysec />
-      <Texh />
-      <Footer />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
